2fecc0554ce33022ccecceb8b5d262637cda7238
SupremeSalvatore <milosposkuricakg@gmail.com>
2021-01-29 18:00:47 +0100

    Adding custom page

---
+++ pages/products/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <LargeCardSection
+            v-for="cardInfo in largeCardSections.slice(0,1)"
+            :key="cardInfo.id"
+            :cardsSection="cardInfo"
+        />
+        <SmallCardSection
+            v-for="cardInfo in smallCardSections"
+            :key="cardInfo.id"
+            :cardsSection="cardInfo"
+        /> 
+    </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from "@/assets/data.js"
+    export default {
+        data(){
+            return {
+                largeCardSections,
+                smallCardSections
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
